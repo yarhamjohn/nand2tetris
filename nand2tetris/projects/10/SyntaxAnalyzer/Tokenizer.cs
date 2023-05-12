a146d73989265f6098e9c17b143824dbfec9a732
John Yarham <yarhamjohn@gmail.com>
2023-05-12 16:59:36 +0100

    add comments

--- nand2tetris/projects/10/SyntaxAnalyzer/Tokenizer.cs
+++ nand2tetris/projects/10/SyntaxAnalyzer/Tokenizer.cs
@@ -34,23 +34,33 @@ public abstract class Tokenizer
             var isWhiteSpace = currentLetter == " ";
             var inStringConstant = currentWord.Length > 0 && currentWord.ToString().First() == '"';
 
+            // The current letter is neither the end of the current word (whitespace)
+            // nor a new symbol token, so should be added to the current word.
             if (!isWhiteSpace && !isSymbol)
             {
                 currentWord.Append(currentLetter);
 
-                var isClosingQuoteMark = currentWord.Length > 1 && currentLetter == "\"";
-                if (isClosingQuoteMark)
+                // However, the current letter is the closing quote mark for the current word
+                // so a string constant token should be created and the current word cleared.
+                if (currentWord.Length > 1 && currentLetter == "\"")
                 {
                     tokens.Add(new StringConstantToken(currentWord.ToString().Replace("\"", "")));
                     currentWord.Clear();
                 }
             }
+            
+            // The current word is an unfinished string constant, so even if though the current letter is
+            // either whitespace or a symbol, it should be added to the current word.
             else if (inStringConstant)
             {
                 currentWord.Append(currentLetter);
             }
+            
+            // The current word is not an unfinished string constant and the current letter is whitespace or a symbol
             else
             {
+                // If the current word is not empty, the current letter marks a new token, or whitespace,
+                // so the current word should be converted into a token and the current word cleared.
                 if (currentWord.Length > 0)
                 {
                     var word = currentWord.ToString();
@@ -70,6 +80,8 @@ public abstract class Tokenizer
                     currentWord.Clear();
                 }
 
+                // Additionally, if the current letter is a symbol, it should be immediately converted into
+                // a token rather than starting a new current word. If whitespace, it should be ignored.
                 if (isSymbol)
                 {
                     tokens.Add(new SymbolToken(Symbols[currentLetter]));
